2365ba06a8d07dee6d9de34233196e08633c06dd
Renam Bulhoes <renamgustavo@live.com>
2022-11-29 14:40:58 -0300

    Iniciando Projeto

---
+++ src/main/java/com/github/renamrgb/meuendereco/MeuEnderecoApplication.java
@@ -0,0 +1,13 @@
+package com.github.renamrgb.meuendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuEnderecoApplication.class, args);
+	}
+
+}
